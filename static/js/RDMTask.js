7d87be9819e9656db2da8466feff3a7b36874d74
Vasundhara Rakesh <vasu9498@gmail.com>
2017-07-08 00:58:59 +0530

    Update RDMTask.js

--- static/js/RDMTask.js
+++ static/js/RDMTask.js
@@ -2,21 +2,19 @@ $(function(){
   $("#Correct").hide();
   $("#Incorrect").hide();
 });
-var startTime = 0;
 var BeginDemo = function() {
-  data.push(['RL',"coherence", "RT"]);
+  data.push(['RL','response', 'coherence', 'RT']);
   RDM();
 }
 var trial_num = [0];
 var RDM = function(){
-  $("#Canvas").show();
+  $("#Canvas").hide();
   $("#Instructions").show();
   $("#Correct").hide();
   $("#Incorrect").hide(); 
   startTime = (new Date()).getTime(); 
   nextTrial();
-  console.log(RL);
-  console.log(coherence);
+  //console.log(coherence);
   paper.install(window);
   if (trial_num > 0) {
     console.log(paper.project.layers);
@@ -24,14 +22,17 @@ var RDM = function(){
   trial_num += 1
   $(document).on('keydown', function stimuli(e){
     if (e.which == 32){
+      startTime = (new Date()).getTime();
+      $("#Instructions").hide();
+      $("#Canvas").show();
       var nums = [];
       for (var i = 0; i < count; i++) {
         nums.push(i);
       }
       shuffling(nums);
-      var First = nums.slice(0, 66);
-      var Second = nums.slice(66, 133);
-      var Third = nums.slice(133, 200);
+      var First = nums.slice(0, 80);
+      var Second = nums.slice(80, 160);
+      var Third = nums.slice(160, 240);
       var full = [First,Second,Third]; 
       paper.setup('Canvas');
       var path = new Path.Circle([view.size.width*Math.random(),view.size.height*Math.random()], size);
@@ -51,16 +52,16 @@ var RDM = function(){
               var current = full[i][j];
               var item = project.activeLayer.children[current];
               if (j < (count/3)*coherence) {
-                item.position.x += (.5)*RL;
-                //item.position.y += 5*norm();
+                item.position.x += (35)*RL;
+                item.position.y += 50*norm();
               } else {
-                item.position.x += 2.5*norm();
-                item.position.y = view.size.height*Math.random(); 
+                item.position.x += ((1-coherence)/coherence)*40*norm();
+                item.position.y += 50*norm(); 
               }
               if (item.bounds.left > view.size.width)
                 item.position.x = +item.bounds.width;
               if (item.bounds.right < 0)
-                item.position.x = -view.size.width;
+                item.position.x = +view.size.width;
               if (item.bounds.bottom < 0)
                 item.position.y = +view.size.height;
               if (item.bounds.top > view.size.height)
@@ -75,20 +76,22 @@ var RDM = function(){
 };
 $(document).on('keydown', function(e){
   if (e.which == 90 || e.which == 77){ 
-    $("Canvas").hide();
+    var endTime = (new Date()).getTime();
+    $("#Instructions").show();
+    $("#Canvas").hide();
     paper.project.remove();
     even = 0;
     if (e.which == 77){
       response = 1;
-      if (RL == "1"){ $("#Correct").show(); } else { $("#Incorrect").show(); }
+      if (RL == "1"){ $("#Correct").show();} else { $("#Incorrect").show(); }
     } else {
       response = -1;
-      if (RL == "-1"){ $("#Correct").show(); } else { $("#Incorrect").show(); }
+      if (RL == "-1"){ $("#Correct").show();} else { $("#Incorrect").show(); }
     }
-    var endTime = (new Date()).getTime();
     RT = endTime - startTime;
     setTimeout(RDM,500);
     trialdata = [RL, response, coherence, RT];
     data.push(trialdata);
+    console.log(RT);
   }     
 });
