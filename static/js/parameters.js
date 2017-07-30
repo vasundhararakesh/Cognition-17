d91a1b84a429b1edefac2010c8064e46f068b7fa
Vasundhara Rakesh <vasu9498@gmail.com>
2017-07-30 16:21:04 +0530

    Create step function

--- static/js/parameters.js
+++ static/js/parameters.js
@@ -6,10 +6,12 @@ var response;
 var data = [];
 //Direction: -1 for left, 1 for right
 var RL = 1;
-var coherence = .15;
 var speed = 80;
 var RL_List = [-1,1];
 var coherence_List = [.1, .2, .3, .4, .5, .6, .7, .8];
+var coherence = 0;
+var dirn = 1;
+var index = 0;
 function norm() {
 	var x = Math.random() + Math.random() + Math.random() + Math.random() + Math.random() + Math.random();
  	x = (x - 3)/3;
@@ -23,6 +25,15 @@ function shuffling(array) {
     array[j] = temp;
   }
   return array;
+}
+function nextStep(){
+  RL = shuffling(RL_List)[0];
+  coherence = coherence_List[index];
+  index += dirn;
+  if (index >= coherence_List.length - 1)
+    dirn = -1;
+  if (index <= 0)
+    dirn = 1;
 }
 function nextTrial() {
   RL = shuffling(RL_List)[0];
